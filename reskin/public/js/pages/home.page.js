b00146ba1692292de73215c59b070885230e70fe
Vu Dinh <Vu.Dinh@S60-153.lanworks.local>
2016-11-25 17:54:57 +0700

    Update code for navigation bar in desktop

--- reskin/public/js/pages/home.page.js
+++ reskin/public/js/pages/home.page.js
@@ -10,5 +10,25 @@
     $(function() {
         loadCarouselForProducts();
         AOS.init();
+
+        $('.search button').on('click', function(e){
+           e.preventDefault();
+           $('.mainnav').removeClass('active');
+           if($('.search-bar').hasClass('active')){
+             $('.search-bar').removeClass('active');
+          } else {
+             $('.search-bar').addClass('active');
+          }
+        });
+
+        $('.nav-menu').on('click', function(e){
+           e.preventDefault();
+           $('.search-bar').removeClass('active');
+           if($('.mainnav').hasClass('active')){
+             $('.mainnav').removeClass('active');
+          } else {
+             $('.mainnav').addClass('active');
+          }
+        });
     });
 })();
